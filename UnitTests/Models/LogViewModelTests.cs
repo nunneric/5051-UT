59bdefb6a7cb302a2c275d9c1d41d6d45750da4b
Eric Nunn <nunneric@seattleu.edu>
2019-06-09 19:08:57 -0700

    Added test case template for LogViewModel_Instantiate_Defaut_Should_Pass()

--- UnitTests/Models/LogViewModelTests.cs
+++ UnitTests/Models/LogViewModelTests.cs
@@ -8,8 +8,13 @@ namespace UnitTests.Models
     public class LogViewModelTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LogViewModel_Instantiate_Defaut_Should_Pass()
         {
+            // Arrange
+
+            // Act
+
+            // Assert
         }
     }
 }
